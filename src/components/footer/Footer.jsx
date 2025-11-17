8fc46207693cbd937c03da36fe766852ca696cad
Nivid Shah <nividshah@Mac.lan>
2025-11-16 16:16:32 -0800

    Fixing website content

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,9 +7,9 @@ import {IoLogoTwitter} from 'react-icons/io'
 const Footer = () => {
   return (
     <footer>
-      <a href='#home' className='footer__logo'>Nivid Shah</a>  
+      <a href='#' className='footer__logo'>Nivid Shah</a>  
       <ul className='permalinks'>
-        <li><a href='#home'>Home</a></li>
+        <li><a href='#'>Home</a></li>
         <li><a href='#about'>About</a></li>
         <li><a href='#experience'>Skills</a></li>
         <li><a href='#services'>Services</a></li>
